2cfd0e28c24952588819a7f7936ac1c3227c4239
MaccSob <71sobmaciej@gmail.com>
2025-01-15 23:44:55 +0100

    messing around

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const {generateRandom, celciusToFahrenheit} = require('./duf');
+
+console.log(`Random Number: ${generateRandom()}`);
+
+
+console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(42)}`);
---
+++ duf.js
@@ -0,0 +1,14 @@
+function generateRandom() {
+    return Math.floor(Math.random() * 100) +1 ;
+}
+
+
+function celciusToFahrenheit(celcius) {
+    return (celcius * 9) / 5 + 32;
+}
+
+
+
+module.exports = {generateRandom,
+    celciusToFahrenheit,
+};
