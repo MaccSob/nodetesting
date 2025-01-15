3eb61c67ba07b16b08560fb0d455cfd2013621a7
MaccSob <71sobmaciej@gmail.com>
2025-01-16 00:04:11 +0100

    first node server

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
-const {generateRandom, celciusToFahrenheit} = require('./duf');
 
-console.log(`Random Number: ${generateRandom()}`);
+import { getPosts, getPostsLength } from './postController.js';
+
+console.log(getPosts());
+console.log(`Post Length: ${getPostsLength()}`)
+
+ // const {generateRandom, celciusToFahrenheit} = require('./duf');
+
+ //console.log(`Random Number: ${generateRandom()}`);
 
 
-console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(42)}`);
+ // console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(42)}`);
