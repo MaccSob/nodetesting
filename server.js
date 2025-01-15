055c41c5a40131ee203868da0095fac9f3bf379c
MaccSob <71sobmaciej@gmail.com>
2025-01-16 00:54:15 +0100

    httpserver

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import http from 'http';
-const PORT = 8000;
-
+const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
      // res.setHeader('Content-type', 'text/html');
 
      // res.statusCode = 404;
-     res.writeHead(500, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify({message: "Server error"}));
+    console.log(req.url);
+    console.log(req.method);
+
+
+     res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end("<h1>Hello My Friend</h1>");
 });
 
 
