02b44b53de5fb3dd8bcfcf5a925071007b84a732
MaccSob <71sobmaciej@gmail.com>
2025-01-17 00:22:30 +0100

    routing

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
 const PORT = process.env.PORT;
-const server = http.createServer((req, res) => {
-     // res.setHeader('Content-type', 'text/html');
-
-     // res.statusCode = 404;
-    console.log(req.url);
-    console.log(req.method);
 
 
-     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end("<h1>Hello My Friend</h1>");
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async(req, res) => {
+
+
+
+
+
+
+    try {
+        // if get request
+        if (req.method ='GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+           
+               } else if(req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+               } else {
+                   throw new Error('Not found');
+               }
+               const data = await fs.readFile(filePath);
+               res.setHeader('Content-type', 'text/html');
+               res.write(data);
+               res.end();
+                    } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'})
+        res.end("<h2>Server error</h2>");
+    }
+
+
+
+    
 });
 
 
